d2bf82d05f033eec64a11431afeba5693200c3cd
Jeffrey Ryan Walker <jryanwalker93@gmail.com>
2020-12-28 17:12:46 -0600

    Adds React Native Navigation

--- App.js
+++ App.js
@@ -1,31 +1,58 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { seedDatabase } from "./database/seed";
 import background from "./assets/background.jpg";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const SignUp = () => {
+  return (
+    <View>
+      <Text>Signup</Text>
+    </View>
+  );
+};
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={background}
-        style={{ width: "100%", height: "100%" }}
-      >
-        <Text
-          style={{
-            color: "#Fbb825",
-            fontSize: 45,
-            paddingTop: "25%",
-            paddingLeft: "10%",
-            margin: "auto",
-            // justifyContent: "center",
-          }}
-        >
-          ATX Beer Passport
-        </Text>
+      <ImageBackground source={background} style={styles.background}>
+        <Text style={styles.title}>ATX Beer Passport</Text>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.buttons}
+            onPress={() => navigation.navigate("Sign Up")}
+          >
+            <Text style={styles.buttonText}>Sign Up!</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttons}>
+            <Text style={styles.buttonText}>Explore</Text>
+          </TouchableOpacity>
+        </View>
+        <StatusBar style="auto" />
       </ImageBackground>
-      <StatusBar style="auto" />
     </View>
+  );
+};
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+        <Stack.Screen name="Sign Up" component={SignUp}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -35,5 +62,34 @@ const styles = StyleSheet.create({
     backgroundColor: "#add8e6",
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    color: "#Fbb825",
+    fontSize: 45,
+    paddingTop: "25%",
+    paddingLeft: "10%",
+    margin: "auto",
+  },
+  background: { width: "100%", height: "100%" },
+  buttonContainer: {
+    width: "100%",
+    height: "10%",
+    flex: 1,
+    flexDirection: "row",
+    marginTop: 20,
+    justifyContent: "space-around",
+  },
+  buttons: {
+    width: "40%",
+    height: "7%",
+    backgroundColor: "rgba(190, 190, 190, 0.5)",
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#C91B26",
+    fontSize: 15,
+    fontWeight: "bold",
   },
 });
